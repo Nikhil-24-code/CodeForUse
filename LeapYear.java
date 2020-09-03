e013d06ef782cd1b11d78c15875258f94177a301
Nikhil Akhouri <69513555+Nikhil-24-code@users.noreply.github.com>
2020-09-04 00:27:24 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+public class LeapYear {
+
+	public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+    System.out.print("Please enter a year to check whether it is leap year or not:  ");    
+	double useryear=sc.nextDouble();
+     int roundyear=Math.abs((int)Math.round(useryear));
+//     System.out.println(roundyear);
+     
+     boolean leapyear=false;
+     if(roundyear % 100 == 0)
+    	 leapyear=true;
+     else if((roundyear % 4 ==0) && (roundyear % 100 != 0))
+    	 leapyear=true;
+      
+     
+    if (leapyear)
+    	System.out.println(roundyear + " is a leap year");
+    else
+    	System.out.println(roundyear + " is not a leap year");
+    
+  
+  }
+
+}
 
