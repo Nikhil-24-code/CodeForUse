71ba4546abe0d2f789ff37b03e451d32a8228052
Nikhil Akhouri <69513555+Nikhil-24-code@users.noreply.github.com>
2020-09-04 00:36:43 +0530

    Update DuplicateInArray.java

--- DuplicateInArray.java
+++ DuplicateInArray.java
@@ -1,8 +1,9 @@
 import java.util.*;
-public class DupTrick {
+public class TestHack {
+
 	public static void main(String ags[])
 	{
-		int i,j;
+		int i,j,temp;
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		int a[]=new int[n];
@@ -10,26 +11,33 @@ public class DupTrick {
 		{
 			a[i]=s.nextInt();
 		}
-		/*System.out.print("Unsorted array input"+" ");
+		System.out.print("Unsorted array input"+" ");
 		for(i=0;i<n;i++)
 		{
 			System.out.print(a[i]+" ");
 		}
-		System.out.println();*/
+		System.out.println();
 		for(i=0;i<n;i++)
 		{
-			int c=0;
 			for(j=i+1;j<n;j++)
 			{
-				if(a[i]==a[j])
+				if(a[i]>a[j])
 				{
-					c++;
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
 				}
 			}
-			if(c>0)
-				System.out.print(a[i]+" ");
-					}
+			
+		}
+		System.out.print("Sorted array output"+" ");
+		for(i=0;i<n;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+		//System.out.println("Smallest and largest");
+		
+		System.out.println(a[0]+","+a[n-1]);
+	}
 }
-	
-}
-
