0c06e6de0e9b82e6aef4c304875ab2f02684342f
Nikhil Akhouri <69513555+Nikhil-24-code@users.noreply.github.com>
2020-09-04 00:34:13 +0530

    Update DuplicateInArray.java

--- DuplicateInArray.java
+++ DuplicateInArray.java
@@ -1,1 +1,35 @@
+import java.util.*;
+public class DupTrick {
+	public static void main(String ags[])
+	{
+		int i,j;
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		/*System.out.print("Unsorted array input"+" ");
+		for(i=0;i<n;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();*/
+		for(i=0;i<n;i++)
+		{
+			int c=0;
+			for(j=i+1;j<n;j++)
+			{
+				if(a[i]==a[j])
+				{
+					c++;
+				}
+			}
+			if(c>0)
+				System.out.print(a[i]+" ");
+					}
+}
+	
+}
 
